282c989f0f04b2e26ad035518dbac2d64583f85b
alecstephens <stephens.alec01@gmail.com>
2022-04-12 12:24:11 -0500

    added some files and set up some basic import statements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
