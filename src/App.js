76d58ecd7633f70c5eb5be213bada2eb7f26ef27
alecstephens <stephens.alec01@gmail.com>
2022-04-18 12:30:04 -0500

    started and finished some of my components

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import axios from 'axios';
+import apiKey from './config';
+
+
+//Flickr Api
+const photoKey = apiKey;
+
+// Components
+import Nav from './components/Nav';
+import PhotoContainer from './components/PhotoContainer';
+import NotFound from './components/NotFound';
+import SearchForm from './components/SearchForm';
+import { render } from "express/lib/response";
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      photos: [],
+      sun: [],
+      moon: [],
+      clouds: [],
+      loading: true,
+      query: ''
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+    this.performSearch('sun');
+    this.performSearch('moon');
+    this.performSearch('clouds');
+  }
+
+  performSearch = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${photoApiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        if(query === 'sun') {
+          this.setState ({
+            sun: responds.data.photos.photo,
+            loading: false
+          })
+        } if(query === 'sun') {
+          this.setState({ sun: responds.data.photos.photo })
+        } if(query === 'moon') {
+          this.setState({ moon: response.data.photos.photo })
+        } if(query === 'clouds') {
+          this.setState({ clouds: response.data.photos.photo })
+        }
+      })
+    
+      .catch(error => {
+        console.log('Error fetching and parsing the data', error);
+      });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <h1>Phtoto Gallery that uses Flickr and React.js</h1>
+        <searchBar onSearch={this.performSearch} />
+        <Nav />
+        <Switch>
+          <Route path="/sun" render={() => <Photo photos={this.state.sun} isLoading={this.state.isLoading} />} />
+          <Route path="/moon" render={() => <Photo photos={this.state.moon} isLoading={this.state.isLoading} />} />
+          <Route path="/clouds" render={() => <Photo photos={this.state.clouds} isLoading={this.state.isLoading} />} />
+          <Rout component={NotFound} />
+        </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  
+  }
+
+}
+
+
+
+export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,2 +1,8 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+
+class SearchForm extends Component {
+    state = {
+        searchText: ''
+    } //show search results and errors
+}
